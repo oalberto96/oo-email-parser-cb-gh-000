fe9f99b0228ec8b8274f01fd82b2188c91ed184a
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-28 18:00:17 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,6 +12,7 @@ class EmailParser
     email_list = emails.split(", ")
     @emails = []
     email_list.each {|email| @emails << email if !@emails.include?(email)}
+    @emails
   end
 
   def parse
